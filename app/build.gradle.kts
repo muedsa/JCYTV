39f9775fe0bbf98edcea87e7842df7982bda04d8
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-04-07 15:47:21 +0800

    update: version 0.0.1-alpha05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.muedsa.jcytv"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.0.1-alpha04"
+        versionCode = 5
+        versionName = "0.0.1-alpha05"
         archivesName = "JCYTV"
         vectorDrawables {
             useSupportLibrary = true
