329c8db9e56d0d17fe3af4b07f18d22211c3810d
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-09-19 17:13:43 +0800

    fix: getVersionInfo

--- app/src/main/kotlin/com/muedsa/uitl/AppUtil.kt
+++ app/src/main/kotlin/com/muedsa/uitl/AppUtil.kt
@@ -8,7 +8,10 @@ object AppUtil {
     fun getVersionInfo(context: Context): String {
         return try {
             val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-            "${packageInfo.versionName}(${packageInfo.longVersionCode})"
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P)
+                "${packageInfo.versionName}(${packageInfo.longVersionCode})"
+            else
+                "${packageInfo.versionName}(${packageInfo.versionCode})"
         } catch (t: Throwable) {
             ""
         }
